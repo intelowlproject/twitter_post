373bb9db9935f29c2648f621aae1ec42eddeddf7
shivam <shivampurohit900@gmail.com>
2023-09-07 12:15:13 +0530

    add test workflow

--- script.py
+++ script.py
@@ -2,18 +2,13 @@ import tweepy
 import os
 
 
-api_key = os.environ['TWITTER_API_KEY']
-api_key_secret = os.environ['TWITTER_API_KEY_SECRET']
-access_token = os.environ['TWITTER_ACCESS_TOKEN']
-access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+client = tweepy.Client(
+    consumer_key=os.environ['TWITTER_API_KEY'],
+    consumer_secret=os.environ['TWITTER_API_KEY_SECRET'],
+    access_token=os.environ['TWITTER_ACCESS_TOKEN'],
+    access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+)
 
-# authenticating using tweepy
-auth = tweepy.OAuthHandler(api_key, api_key_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
 
-# tweet content
 tweet_content = "Published a new release of my project! #ReleaseNotes"
-
-# Post the tweet
-api.update_status(tweet_content)
+client.create_tweet(text = tweet_content)
