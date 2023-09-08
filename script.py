e8abd839911e90f8b25d9931f4404cd5f2078134
shivam <shivampurohit900@gmail.com>
2023-09-08 11:51:29 +0530

    modify to support publishing

--- script.py
+++ script.py
@@ -1,14 +1,12 @@
 import tweepy
 import os
 
-
 client = tweepy.Client(
-    consumer_key=os.environ['TWITTER_API_KEY'],
-    consumer_secret=os.environ['TWITTER_API_KEY_SECRET'],
-    access_token=os.environ['TWITTER_ACCESS_TOKEN'],
-    access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+    consumer_key=os.environ['api_key'],
+    consumer_secret=os.environ['api_key_secret'],
+    access_token=os.environ['access_token'],
+    access_token_secret=os.environ['access_token_secret']
 )
 
-
-tweet_content = "Published a new release of my project! #ReleaseNotes"
+tweet_content = os.environ['status']
 client.create_tweet(text = tweet_content)
